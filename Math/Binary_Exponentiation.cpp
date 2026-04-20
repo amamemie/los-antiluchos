d4ac8511e039f7b4b36899766d771c474cf24336
amamemie <luisrojas10927406@gmail.com>
2026-04-19 21:35:33 -0400

    Update Binary_Exponentiation.cpp

--- Math/Binary_Exponentiation.cpp
+++ Math/Binary_Exponentiation.cpp
@@ -1,10 +1,9 @@
-const long long mod = 998244353;
-long long pote(long long a, long long b){
+long long pote(long long a, long long b, long long mod){
     long long ans = 1ll;
     while(b){
         if(b&1) ans = (ans * a)%mod;
-        b>>=1;
         a = (a * a)%mod;
+        b>>=1;
     }
     return ans;
 }
