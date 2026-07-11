3f98a03b4061352d32badeb141703a009ce9737a
Waldo Caballero <yasirwaldocaba@gmail.com>
2026-07-11 10:57:27 -0400

    Añadido AJO CARAJO para FREQ

--- Strings/Aho_Corasick.cpp
+++ Strings/Aho_Corasick.cpp
@@ -94,3 +94,91 @@ void solve(){
         }
     }
 }
+
+//-------------------------------------------------------------------------
+
+//AhoCorasick para Frecuencias
+const int E = 26;
+const int N = 1e6+10;
+int nodoTerminal[N];
+int cur;
+struct AC {
+    int nodes;
+    vector<int>suf, freq, by_level;
+    vector<bool>terminal;
+    vector<array<int,E>>go;
+
+    AC(){
+        add_node();
+        nodes = 1;
+    }
+    void add_node(){
+        terminal.emplace_back();
+        suf.emplace_back();
+        go.emplace_back();
+        freq.emplace_back();
+    }
+
+    void insert(string &s){
+        int pos = 0;
+        for(char ch:s){
+            int c = ch - 'a';
+            if(go[pos][c] == 0){
+                go[pos][c] = nodes++;
+                add_node();
+            }
+            pos = go[pos][c];
+        }
+        terminal[pos] = true;
+        nodoTerminal[cur] = pos;
+    }
+
+    void build(){
+        queue<int>Q;
+        Q.emplace(0);
+        while(!Q.empty()){
+            int u = Q.front();
+            Q.pop();
+            by_level.emplace_back(u);
+            for(int c = 0; c < E; c++){
+                if(go[u][c]){
+                    int v = go[u][c];
+                    Q.emplace(v);
+                    suf[v] = u == 0 ? 0 : go[suf[u]][c];
+                }
+                else{
+                    go[u][c] = u == 0 ? 0 : go[suf[u]][c];
+                }
+            }
+        }
+    }
+};
+
+void solve(){
+    int n;
+    cin >> n;
+    AC ac;
+    repl(i,0,n){
+        string s;
+        cin >> s;
+        cur = i;
+        ac.insert(s);
+    }
+    string t;
+    cin >> t;
+    ac.build();
+    int p = 0;
+    for(char ch:t){
+        int c = ch-'a';
+        p = ac.go[p][c];
+        ac.freq[p]++;
+    }
+    for(int i = (int)ac.by_level.size()-1; i > 0; i--){
+        int x = ac.by_level[i];
+        ac.freq[ac.suf[x]] += ac.freq[x];
+    }
+    repl(i,0,n){
+        int termi = nodoTerminal[i];
+        cout << ac.freq[termi] << '\n';
+    }
+}
