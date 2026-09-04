13eb3ef617a7904305f80491eaf4292184b94f89
amamemie <luisrojas10927406@gmail.com>
2026-09-04 12:33:39 -0400

    ST iterativo

--- Data Structures/Segment_Tree_Iterativo.cpp
+++ Data Structures/Segment_Tree_Iterativo.cpp
@@ -3,6 +3,10 @@ struct info
     int num;
 };
 
+info ope(info u, info v){
+    return {min(u.num, v.num)};
+}
+
 int sz;
 vector<info> ST;
 
@@ -14,18 +18,18 @@ void build(int n) {
 
 void update(int pos, int val) {
     pos += sz;
-    ST[pos].num = min(ST[pos].num, val);
+    ST[pos] = ope(ST[pos], {val});
     for (pos /= 2; pos > 0; pos /= 2) {
-        ST[pos].num = min(ST[2 * pos].num, ST[2 * pos + 1].num);
+        ST[pos] = ope(ST[2 * pos], ST[2 * pos + 1]);
     }
 }
 
 info get(int l, int r) {
-    int res = 1000000000;
+    info res = {1000000000};
     for (l += sz, r += sz + 1; l < r; l /= 2, r /= 2) {
-        if (l & 1) res = min(res, ST[l++].num);
-        if (r & 1) res = min(res, ST[--r].num);
+        if (l & 1) res = ope(res, ST[l++]);
+        if (r & 1) res = ope(res, ST[--r]);
     }
-    return {res};
+    return res;
 }
-//Consultas exactamente igual que el recursivo
+//Las operaciones y consultas son iguales al recursivo, 0-index
